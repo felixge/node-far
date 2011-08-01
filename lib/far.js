felixge/node-far

commit cec128cdadec568991b250d435d202d87ec61b52

No interval timer if verbose mode is on

lib/far.js
@@ -64,7 +64,9 @@ Far.prototype.execute = function() {
   this._start = new Date;
   this.findSync();
 
-  this._intervalTimer = setInterval(this._printStatus.bind(this), this._interval);
+  if (this.verbose == 0) {
+    this._intervalTimer = setInterval(this._printStatus.bind(this), this._interval);
+  }
   this._executeNext();
 };
 

commit c337a90766ee81cc858cb62f279e23b5dfd8f4ad

Fix timer when reporting w/ verbose = 0
Broken in cec128cdadec568991b250d435d202d87ec61b52

lib/far.js
@@ -64,7 +64,7 @@ Far.prototype.execute = function() {
   this._start = new Date;
   this.findSync();
 
-  if (this.verbose == 0) {
+  if (this.verbose > 0) {
     this._intervalTimer = setInterval(this._printStatus.bind(this), this._interval);
   }
   this._executeNext();